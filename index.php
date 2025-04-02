847ec883e2453e799ff9a362cd4b6d502024740b
Rajan <rajan808172@gmail.com>
2025-04-02 18:31:55 +0530

    s

--- index.php
+++ index.php
@@ -5,10 +5,7 @@ require_once('./common/sidebar.php'); ?>
 	<div class="content">
 
 		<div class="d-flex align-items-center justify-content-between flex-wrap gap-3 mb-2">
-			<div class="mb-3">
-				<h1 class="mb-1">Welcome, Admin</h1>
-				<p class="fw-medium">You have <span class="text-primary fw-bold">200+</span> Orders, Today</p>
-			</div>
+			
 
 		</div>
 	</div>
