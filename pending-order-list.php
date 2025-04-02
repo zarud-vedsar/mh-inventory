979618f504e3d293158463b838d6240204147066
Siyoka Okaya <rajansahu.vedsar@gmail.com>
2025-04-02 13:37:34 +0530

    s

--- pending-order-list.php
+++ pending-order-list.php
@@ -18,27 +18,29 @@ $recycle = $action->db->validateGetData('recycle') ?: null;
             <ul class="table-top-head">
 
                 <li>
-                    <a data-bs-toggle="tooltip" data-bs-placement="top" title="Collapse" id="collapse-header"><i class="ti ti-chevron-up"></i></a>
+                    <a data-bs-toggle="tooltip" data-bs-placement="top" title="Collapse" id="collapse-header"><i
+                            class="ti ti-chevron-up"></i></a>
                 </li>
             </ul>
             <div class="am-page-upr-btn">
                 <div class="left-btn">
                     <div class="page-btn">
-                        <a href="./pending-orders.php" class="btn btn-primary"><i class="ti ti-circle-plus me-1"></i>Pending orders</a>
+                        <a href="./pending-orders.php" class="btn btn-primary"><i class="ti ti-circle-plus me-1"></i>New
+                            Pending orders</a>
                     </div>
                     <div class="page-btn">
-						<?php if ($recycle) { ?>
-							<a href="./pending-order-list.php" class="btn btn-secondary">
-								<i data-feather="eye-off"></i>
-								Hide Recycle Bin
-							</a>
-						<?php } else { ?>
-							<a href="./pending-order-list.php?recycle=true" class="btn btn-danger">
-								<i data-feather="trash-2"></i>
-								Show Recycle Bin
-							</a>
-						<?php } ?>
-					</div>
+                        <?php if ($recycle) { ?>
+                            <a href="./pending-order-list.php" class="btn btn-secondary">
+                                <i data-feather="eye-off"></i>
+                                Hide Recycle Bin
+                            </a>
+                        <?php } else { ?>
+                            <a href="./pending-order-list.php?recycle=true" class="btn btn-danger">
+                                <i data-feather="trash-2"></i>
+                                Show Recycle Bin
+                            </a>
+                        <?php } ?>
+                    </div>
                 </div>
                 <div class="page-btn goBack">
                     <a href="#" class="btn btn-secondary"><i data-feather="arrow-left" class="me-2"></i>Back</a>
@@ -61,68 +63,73 @@ $recycle = $action->db->validateGetData('recycle') ?: null;
                     <table class="table datatable">
                         <thead class="thead-light">
                             <tr>
-                                <th>#<img src="./assets/img/svg_icons/right-left-arrow.svg" alt="" class="am-sort-icon"></th>
-                                <th> Order Date<img src="./assets/img/svg_icons/right-left-arrow.svg" alt="" class="am-sort-icon"></th>
-                                <th> Party<img src="./assets/img/svg_icons/right-left-arrow.svg" alt="" class="am-sort-icon"></th>
-                                <th> Dispatched Status<img src="./assets/img/svg_icons/right-left-arrow.svg" alt="" class="am-sort-icon"></th>
-                                <th> Dispatched Date<img src="./assets/img/svg_icons/right-left-arrow.svg" alt="" class="am-sort-icon"></th>
+                                <th>#<img src="./assets/img/svg_icons/right-left-arrow.svg" alt="" class="am-sort-icon">
+                                </th>
+                                <th> Order Date<img src="./assets/img/svg_icons/right-left-arrow.svg" alt=""
+                                        class="am-sort-icon"></th>
+                                <th> Party<img src="./assets/img/svg_icons/right-left-arrow.svg" alt=""
+                                        class="am-sort-icon"></th>
+                                <th> Dispatched Status<img src="./assets/img/svg_icons/right-left-arrow.svg" alt=""
+                                        class="am-sort-icon"></th>
+                                <th> Dispatched Date<img src="./assets/img/svg_icons/right-left-arrow.svg" alt=""
+                                        class="am-sort-icon"></th>
 
                                 <th class="no-sort">Action</th>
-                    
+
                             </tr>
                         </thead>
                         <tbody>
-                         <?php 
-                            $sql="SELECT aimo_order.*,aimo_party.party_name,aimo_party.party_phone FROM aimo_order JOIN aimo_party ON aimo_party.id=aimo_order.pending_party WHERE 1";
+                            <?php
+                            $sql = "SELECT aimo_order.*,aimo_party.party_name,aimo_party.party_phone FROM aimo_order JOIN aimo_party ON aimo_party.id=aimo_order.pending_party WHERE 1";
                             $deleteStatus = $recycle ? 1 : 0;
                             $sql .= " AND aimo_order.deleteStatus = {$deleteStatus}";
                             $orderlisting = $action->db->sql($sql);
-                            
-                            if($orderlisting){
+
+                            if ($orderlisting) {
 
                                 $sr = 1;
-                                foreach($orderlisting as $od){ 
-                          ?>
-                            <tr>
-                               <td><?= $sr++; ?></td> 
-                               <td class="text-gray-9"><?= $action->db->indiandate($od['order_date']); ?></td>
-                               <td class="text-gray-9"><?= $od['party_name'];?></td>
-                               <td>
-                                 <?php 
-                                  if(empty($od['dispatched_date']) || $od['dispatched_date'] == '0000-00-00'){
-                                       echo "<span class='text-warning'> Pending <span>";
-                                  }else{
-                                    echo "<span class='text-success'> Dispatched <span>";
-                                  }
-                                 ?>
-                               </td>
-                               <td class="text-gray-9"><?= $action->db->indiandate($od['dispatched_date']); ?></td> 
-                               <td class="action-table-data d-flex align-items-center">
-											
-											<div class="edit-delete-action">
-												<?php if (!$recycle) { ?>
-													<a href="./pending-orders.php?id=<?= $od['id']; ?>"
-														class="p-2 border-0 bg-transparent">
-														<i data-feather="edit" class="feather-edit text-warning"></i>
-													</a>
-												<?php }
-												if ($deleteStatus == 1) { ?>
-													<button type="button" class="p-2 border-0 bg-transparent recover-item-order"
-														data-delid="<?= @$od['id']; ?>">
-														<i data-feather="refresh-ccw" class="feather-refresh-ccw text-success"></i>
-													</button>
-												<?php } else { ?>
-													<button type="button" class="p-2 border-0 bg-transparent delete-item-order"
-														data-delid="<?= @$od['id']; ?>">
-														<i data-feather="trash-2" class="feather-trash-2 text-danger"></i>
-													</button>
-												<?php } ?>
-											</div>
-										</td>
-                             </tr>
-                           
-                          <?php
-                                } 
+                                foreach ($orderlisting as $od) {
+                                    ?>
+                                    <tr>
+                                        <td><?= $sr++; ?></td>
+                                        <td class="text-gray-9"><?= $action->db->indiandate($od['order_date']); ?></td>
+                                        <td class="text-gray-9"><?= $od['party_name']; ?></td>
+                                        <td>
+                                            <?php
+                                            if (empty($od['dispatched_date']) || $od['dispatched_date'] == '0000-00-00') {
+                                                echo "<span class='text-warning'> Pending <span>";
+                                            } else {
+                                                echo "<span class='text-success'> Dispatched <span>";
+                                            }
+                                            ?>
+                                        </td>
+                                        <td class="text-gray-9"><?= $action->db->indiandate($od['dispatched_date']); ?></td>
+                                        <td class="action-table-data d-flex align-items-center">
+
+                                            <div class="edit-delete-action">
+                                                <?php if (!$recycle) { ?>
+                                                    <a href="./pending-orders.php?id=<?= $od['id']; ?>"
+                                                        class="p-2 border-0 bg-transparent">
+                                                        <i data-feather="edit" class="feather-edit text-warning"></i>
+                                                    </a>
+                                                <?php }
+                                                if ($deleteStatus == 1) { ?>
+                                                    <button type="button" class="p-2 border-0 bg-transparent recover-item-order"
+                                                        data-delid="<?= @$od['id']; ?>">
+                                                        <i data-feather="refresh-ccw" class="feather-refresh-ccw text-success"></i>
+                                                    </button>
+                                                <?php } else { ?>
+                                                    <button type="button" class="p-2 border-0 bg-transparent delete-item-order"
+                                                        data-delid="<?= @$od['id']; ?>">
+                                                        <i data-feather="trash-2" class="feather-trash-2 text-danger"></i>
+                                                    </button>
+                                                <?php } ?>
+                                            </div>
+                                        </td>
+                                    </tr>
+
+                                    <?php
+                                }
                             }
                             ?>
                         </tbody>
--- pending-orders.php
+++ pending-orders.php
@@ -90,7 +90,6 @@ if ($action->db->validateGetData('id') && filter_var($action->db->validateGetDat
                 </div>
             </div>
             <ul class="table-top-head">
-
                 <li>
                     <a data-bs-toggle="tooltip" data-bs-placement="top" title="Collapse" id="collapse-header"><i
                             class="ti ti-chevron-up"></i></a>
@@ -98,7 +97,7 @@ if ($action->db->validateGetData('id') && filter_var($action->db->validateGetDat
             </ul>
             <div class="am-page-upr-btn">
                 <div class="left-btn">
-                    <div class="page-btn">
+                   <div class="page-btn">
                         <a href="./party-list.php" class="btn btn-info"><i class="fa-solid fa-list-ul me-1"></i></i>
                             Order List</a>
                     </div>
