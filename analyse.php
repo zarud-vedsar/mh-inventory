ca4e19856fb3811534eadf9e574c1a205bf8ec44
Siyoka Okaya <rajansahu.vedsar@gmail.com>
2025-04-02 16:21:39 +0530

    s

---
+++ analyse.php
@@ -0,0 +1,131 @@
+<?php
+require_once('./common/head.php');
+require_once('./common/header.php');
+require_once('./common/sidebar.php');
+?>
+<div class="page-wrapper">
+    <div class="content">
+        <div class="page-header">
+            <div class="add-item d-flex">
+                <div class="page-title">
+                    <h4 class="fw-bold">Analyse Item Quantity</h4>
+                </div>
+            </div>
+            <ul class="table-top-head">
+                <li>
+                    <a data-bs-toggle="tooltip" data-bs-placement="top" title="Collapse" id="collapse-header"><i
+                            class="ti ti-chevron-up"></i></a>
+                </li>
+            </ul>
+            <div class="am-page-upr-btn">
+                <div class="left-btn">
+                </div>
+                <div>
+                    <div class="page-btn goBack">
+                        <a href="#" class="btn btn-secondary"><i data-feather="arrow-left" class="me-2"></i>Back</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="row align-items-center">
+                            <div class="col-md-9">
+                                <form method="get">
+                                    <div class="row">
+                                        <div class="col-md-6">
+                                            <label for="item_name" class="form-label">Select Item <span
+                                                    class="text-danger">*</span></label>
+                                            <select name="item_id" required class="select2 form-select item_id">
+                                                <option value="">Select</option>
+                                                <?php
+                                                $fetchItemDropdown = $action->get_admin_function->fetchItemDropdown();
+                                                if ($fetchItemDropdown) {
+                                                    foreach ($fetchItemDropdown as $item) { ?>
+                                                        <option value="<?= $item['id']; ?>"><?= $item['item_name']; ?>
+                                                            (<?= $item['print_name']; ?>)</option>
+                                                    <?php }
+                                                } ?>
+                                            </select>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <label class="form-label">Dispatched Date <span
+                                                    class="text-danger">*</span></label>
+                                            <div class="input-groupicon calender-input">
+                                                <input type="date" name="dispatched_date" required class="form-control"
+                                                    value="<?= @$_GET['dispatched_date']; ?>">
+                                            </div>
+                                        </div>
+                                        <div class="col-md-2 d-flex justify-content-center align-items-center">
+                                            <button type="submit" class="btn btn-primary mt-4">Apply</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="col-md-3">
+                                <div class="search-set">
+                                    <div class="search-input">
+                                        <span class="btn-searchset"><i
+                                                class="ti ti-search fs-14 feather-search"></i></span>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table datatable">
+                                <thead class="thead-light">
+                                    <tr>
+
+                                        <th>#<img src="./assets/img/svg_icons/right-left-arrow.svg" alt=""
+                                                class="am-sort-icon">
+                                        </th>
+                                        <th> Item <img src="./assets/img/svg_icons/right-left-arrow.svg" alt=""
+                                                class="am-sort-icon">
+                                        </th>
+                                        <th> Dispatched Date<img src="./assets/img/svg_icons/right-left-arrow.svg"
+                                                alt="" class="am-sort-icon"></th>
+                                        <th> Quantity<img src="./assets/img/svg_icons/right-left-arrow.svg" alt=""
+                                                class="am-sort-icon"></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    if ($action->db->validateGetData('item_id') && $action->db->validateGetData('dispatched_date')) {
+                                        $dispatched_date = $action->db->validateGetData('dispatched_date');
+                                        $item_id = $action->db->validateGetData('item_id');
+                                        $qty = $action->db->sql("SELECT SUM(CAST(JSON_UNQUOTE(JSON_EXTRACT(item, '$[0].item_qty')) AS UNSIGNED)) AS total_item_qty
+FROM aimo_order
+WHERE dispatched_date = '2025-04-09'
+  AND JSON_UNQUOTE(JSON_EXTRACT(item, '$[0].item_id')) = '1'
+UNION ALL
+SELECT 
+    SUM(CAST(JSON_UNQUOTE(JSON_EXTRACT(item, '$[1].item_qty')) AS UNSIGNED)) AS total_item_qty
+FROM aimo_order
+WHERE dispatched_date = '{$dispatched_date}'
+  AND JSON_UNQUOTE(JSON_EXTRACT(item, '$[1].item_id')) = {$item_id}");
+                                        $itemName = $action->db->sql("SELECT item_name FROM aimo_item WHERE id = {$item_id}");
+                                    }
+                                    ?>
+                                    <tr>
+                                        <td>1</td>
+                                        <td><?php echo @$itemName[0]['item_name']; ?></td>
+                                        <td><?= $action->db->indiandate($dispatched_date); ?></td>
+                                        <td><?php echo @$qty[0]['total_item_qty']; ?></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+require_once('./common/footer.php');
+?>
